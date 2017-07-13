8107adc96a9d113e15a7295b72234578f3dfdb3d
David Williamson <d.j.williamson@soton.ac.uk>
2017-07-13 10:09:16 +0100

    removing useless output files

--- raytests/benchmark.sh
+++
@@ -1,7 +0,0 @@
-for i in 64 32 16 8 4 2
-do
-    export OMP_NUM_THREADS=$i
-    #echo $OMP_NUM_THREADS
-    echo "nprocs=" $i
-    ./rebeamcomp
-done
--- raytests/gizmo_hdf5_input_test.f90
+++
@@ -1,7 +0,0 @@
-
-program gizmo_hdf5_input_test
-    use gizmo_hdf5_input
-    implicit none
-    call open_hdf5("/export/1/djw/gizmo_public/disc_nocool_midres_out/snapshort_096.hdf")
-end program gizmo_hdf5_input_test
-
