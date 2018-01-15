3eb8ba73d87d67b2b881e31d8567ba8bea77f291
D.J.Williamson <djw1g16@srv01921.soton.ac.uk>
2018-01-15 15:00:34 +0000

    fixed rho and fixed Q stuff

--- gadget_gizmo_simple_ics/src/disc_ICs.f90
+++ gadget_gizmo_simple_ics/src/disc_ICs.f90
@@ -13,15 +13,39 @@ module IC_parameters
     
     real(kind=8) :: hernquist_mass,hernquist_scale
     
+    real(kind=8) :: Q_target, rho_target
+    
     integer :: pot_type
     integer, parameter :: FLAT_POT=0, HERNQUIST_POT=1
     
     logical :: selfgrav
+    logical :: constant_surf
 
     character(len=256) :: position_file
 
 
+    real(kind=8), parameter :: cooled_sound_speed = .67d0
+
+
 end module IC_parameters
+
+module Units
+    real(kind=8), parameter :: pi = 4.d0*atan(1.d0)
+    
+    ! gadget/gizmo internal units
+    real(kind=8), parameter :: munit_cgs = 1.989d43 ! 10^10 solar masses
+    real(kind=8), parameter :: tunit_cgs = 3.08568d16 ! just under a Gyr
+    real(kind=8), parameter :: runit_cgs = 3.0857d21 ! 1 kpc
+    real(kind=8), parameter :: uunit_cgs = 1.d10 ! 1e10 erg/g
+    !real(kind=8), parameter :: G = 6.67408d-8 * tunit_cgs**2 * munit_cgs / runit_cgs**3 ! G=43007.1
+    real(kind=8), parameter :: G = 43007.1 ! from Gizmo output, consist with above units
+    
+    real(kind=8), parameter :: molecular_mass = 4./(1.+3.*.76), proton_mass_cgs = 1.6726d-24
+    real(kind=8), parameter :: gamma_minus_one = 5./3.-1., boltzmann_cgs = 1.38066d-16
+    real(kind=8), parameter :: u_to_TK = gamma_minus_one/boltzmann_cgs*(molecular_mass*proton_mass_cgs)
+
+    real(kind=8), parameter :: nunit = munit_cgs/runit_cgs**3/molecular_mass/proton_mass_cgs
+end module Units
 
 program disc_ICs
     use gadget_ics_IO
@@ -56,6 +80,10 @@ program disc_ICs
     a_scale=0.01d0 ! in kpc/CM_PER_MPC/UnitLength_in_cm;
     c_scale=0.0001d0 ! in kpc/CM_PER_MPC/UnitLength_in_cm;
     inwards_v=0.
+    
+    Q_target = -1 ! i.e. don't fit Q
+    rho_target = -1 ! i.e. don't fit density
+    constant_surf = .false. ! if true, Q_target is *minimum* Q, and try to make surface density & 3d density constant 
     
     position_file = "" ! generate positions randomly
     
@@ -130,6 +158,13 @@ program disc_ICs
                     read(invalue,*) c_scale
                 case("inv")
                     read(invalue,*) inwards_v
+                    
+                case("Q")
+                    read(invalue,*) Q_target
+                case("CONST_SURF")
+                    constant_surf = .true.
+                case("nH")
+                    read(invalue,*) rho_target
                 
                 case("flat")
                     pot_type = FLAT_POT
@@ -298,9 +333,182 @@ subroutine set_flags
 
 end subroutine 
 
+function epicyclic_parameter(r) result(kappa)
+    use IC_parameters
+    use Units
+    implicit none
+    
+    real(kind=8) :: r
+    
+    real(kind=8) :: kappa
+
+    if ( pot_type/=HERNQUIST_POT ) then
+        print*,"CAN ONLY USE CONSTANT Q SURFACE DENSITY PROFILE WITH HERNQUIST POTENTIAL"
+        stop
+    endif
+    
+    kappa = 0.
+
+    ! smbh component
+    kappa = m_smbh/(r**2+c_scale**2) * (2*c_scale**2/(r**2+c_scale**2)+1)
+
+    ! hernquist component
+    kappa = kappa + hernquist_mass*(hernquist_scale+r)**2*(2*hernquist_scale/(hernquist_scale+r)+1)
+    
+    kappa = kappa * G/r
+    
+    kappa = sqrt(kappa)
+    
+    return
+end function
+    
+
+function constant_q_surf(r,cs,Q) result(surf)
+    use IC_parameters
+    use Units
+    implicit none
+
+    real(kind=8) :: r,cs,Q
+    real(kind=8) :: surf
+    real(kind=8) :: epicyclic_parameter
+    
+    surf = cs*epicyclic_parameter(r)/pi/G/Q
+    
+    return
+end function
+
+! assume surface density decreases monotonically with distance at constant Q
+subroutine calc_max_q_surf_profile
+    use IC_parameters
+    use gadget_ics_IO
+    use Units
+    implicit none
+
+    real(kind=8) :: constant_q_surf
+
+    
+    real(kind=8) :: r_guess, r_lowerbound,r_upperbound
+    real(kind=8) :: mass_guess
+    real(kind=8) :: sensitivity, error
+    real(kind=8), dimension(p_data%ng,3) :: rans
+    
+    sensitivity = 1.d-4
+    
+    error = sensitivity+1.d0
+    
+    r_lowerbound = disc_inner_rad
+    r_upperbound = disc_inner_rad*1.d3
+    
+    do while ( error>=sensitivity )
+        r_guess = (r_lowerbound+r_upperbound)*.5d0
+        mass_guess = constant_q_surf(r_guess,cooled_sound_speed,Q_target)
+        mass_guess = mass_guess * pi * (r_guess**2 - disc_inner_rad**2)
+        error = abs((mass_guess-mtot)/mtot)
+
+        !print *,r_lowerbound,r_guess,r_upperbound,mass_guess,mtot,error
+
+        if ( mass_guess>mtot ) then
+            r_upperbound = r_guess
+        else
+            r_lowerbound = r_guess
+        endif
+    end do
+    
+    dense_index = 0.d0
+    disc_rad = r_guess
+    
+    disc_thick = constant_q_surf(r_guess,cooled_sound_speed,Q_target)/rho_target/2.d0
+end subroutine
+
+subroutine calc_const_q_surf_profile(rans)
+    use IC_parameters
+    use gadget_ics_IO
+    use Units
+    implicit none
+    
+    integer :: ibin
+    integer, parameter :: nbin=100
+    
+    real(kind=8) :: constant_q_surf
+
+
+    real(kind=8) :: r_inner,r_outer,rbin
+    real(kind=8) :: m_so_far,mbin,bin_surf
+    
+    
+    real(kind=8) :: stepsize
+    
+    real(kind=8), dimension(:), allocatable :: fbin
+    real(kind=8), dimension(p_data%ng,3) :: rans
+    
+    integer :: ip
+    
+    stepsize = (disc_rad-disc_inner_rad)/nbin
+    r_inner = disc_inner_rad
+    m_so_far = 0.d0
+    
+    
+    ! recalculate disc outer radius, given surface density profile from Q=constant criterion
+    do while (m_so_far<mtot)
+        r_outer = r_inner+stepsize
+        rbin = (r_inner+r_outer)/2.d0
+        bin_surf = constant_q_surf(rbin,cooled_sound_speed,Q_target)
+        mbin = bin_surf*pi*(r_outer**2-r_inner**2)
+        m_so_far = m_so_far + mbin
+!         print *,rbin*1d3,bin_surf*(1d4),mbin*1d10,m_so_far*1d10
+        r_inner = r_outer
+    end do
+
+    if ( m_so_far==mtot ) then
+        disc_rad = r_outer
+    else
+        ! interpolate to guess final position
+        disc_rad = sqrt((mtot-m_so_far+mbin)/pi/bin_surf+(r_outer-stepsize)**2)
+    endif
+    
+
+    ! redo integration, tracking cumulative mass to make probability frequency
+    allocate(fbin(nbin))
+    
+    stepsize = (disc_rad-disc_inner_rad)/nbin
+    r_inner = disc_inner_rad
+    m_so_far = 0.d0
+    do ibin=1,nbin
+        r_outer = r_inner+stepsize
+        rbin = (r_inner+r_outer)/2.d0
+        bin_surf = constant_q_surf(rbin,cooled_sound_speed,Q_target)
+        mbin = bin_surf*pi*(r_outer**2-r_inner**2)
+        m_so_far = m_so_far + mbin
+        fbin(ibin) = m_so_far
+        r_inner = r_outer
+    end do
+    
+    fbin = fbin/fbin(nbin)
+    
+    do ip=1,p_data%ng
+        ibin = 1
+        do while (fbin(ibin)<rans(ip,1) .and. ibin<=nbin)
+            ibin = ibin + 1
+        end do
+        r_inner = disc_inner_rad+(ibin-1)*stepsize
+        if ( ibin==1 ) then
+            rans(ip,1) = r_inner+rans(ip,1)/fbin(1)*stepsize
+        else
+            rans(ip,1) = r_inner+(rans(ip,1)-fbin(ibin-1))/(fbin(ibin)-fbin(ibin-1))*stepsize
+        endif
+        if ( rho_target>=1 ) then
+            ! set thickness to fit rho
+            bin_surf = constant_q_surf(rans(ip,1),cooled_sound_speed,Q_target)
+            rans(ip,3) = (rans(ip,3)-.5d0)*(bin_surf/rho_target)
+        endif
+    end do
+    
+end subroutine
+
 subroutine disc_locs(tproffile)
     use gadget_ics_IO
     use IC_parameters
+    use Units
     implicit none
     
     integer :: ip
@@ -311,20 +519,6 @@ subroutine disc_locs(tproffile)
     real(kind=8), dimension(:),allocatable :: rad2d, z_over_r, rad_transformed
     real(kind=8), dimension(:,:),allocatable :: r_transformed
     
-    real(kind=8), parameter :: pi = 4.d0*atan(-1.d0)
-    
-    ! gadget/gizmo internal units
-    real(kind=8), parameter :: munit_cgs = 1.989d43 ! 10^10 solar masses
-    real(kind=8), parameter :: tunit_cgs = 3.08568d16 ! just under a Gyr
-    real(kind=8), parameter :: runit_cgs = 3.0857d21 ! 1 kpc
-    real(kind=8), parameter :: uunit_cgs = 1.d10 ! 1e10 erg/g
-    !real(kind=8), parameter :: G = 6.67408d-8 * tunit_cgs**2 * munit_cgs / runit_cgs**3 ! G=43007.1
-    real(kind=8), parameter :: G = 43007.1 ! from Gizmo output, consist with above units
-    
-    real(kind=8), parameter :: molecular_mass = 4./(1.+3.*.76), proton_mass_cgs = 1.6726d-24
-    real(kind=8), parameter :: gamma_minus_one = 5./3.-1., boltzmann_cgs = 1.38066d-16
-    real(kind=8), parameter :: u_to_TK = gamma_minus_one/boltzmann_cgs*(molecular_mass*proton_mass_cgs)
-    
     ! unit conversions
     m_smbh = m_smbh/1.d10
     hernquist_mass = hernquist_mass/1.d10
@@ -332,7 +526,10 @@ subroutine disc_locs(tproffile)
     inwards_v = inwards_v*1.d5*(tunit_cgs/runit_cgs) ! tunit/runit is basically 1 km/s anyway
     vloop0 = vloop0*1.d5*(tunit_cgs/runit_cgs) ! tunit/runit is basically 1 km/s anyway
     ! a_scale and c_scale are already in kpc
-    
+
+    if ( rho_target>0. ) then
+        rho_target = rho_target / nunit
+    endif
     
     allocate(rans(p_data%ng,3))
     call random_number(rans) ! generate random numbers
@@ -343,16 +540,32 @@ subroutine disc_locs(tproffile)
         !rans(:,1) = dsqrt(rans(:,1))*disc_rad ! no inner radius
         !rans(:,1) = dsqrt(rans(:,1)*(disc_rad**2-disc_inner_rad**2)+disc_inner_rad**2)  
     
-        ! radius - for power-law density
-        rans(:,1) = (  rans(:,1)*(disc_rad**(dense_index+2.d0)-disc_inner_rad**(dense_index+2.d0)) &
+        if ( Q_target>0. .and. .not. constant_surf) then
+            call calc_const_q_surf_profile(rans)
+            !call calc_max_q_surf_profile
+        else
+            if ( constant_surf ) then
+                if ( Q_target<=0. ) then
+                    print *,"Need to set Q_target to have constant surf"
+                    stop
+                endif
+                
+                call calc_max_q_surf_profile
+            endif
+            ! radius - for power-law density
+            rans(:,1) = (  rans(:,1)*(disc_rad**(dense_index+2.d0)-disc_inner_rad**(dense_index+2.d0)) &
                         +disc_inner_rad**(dense_index+2.d0)   )**(1./(dense_index+2.d0))  
+        endif
+
 
         !rans(:,1) = (rans(:,1)*(disc_rad**3-disc_inner_rad**3)+disc_inner_rad**3)**(1.d0/3.d0) ! with flaring disc
         ! theta coordinate
         rans(:,2) = rans(:,2)*2.*pi
         ! height above/below plane
         !rans(:,3) = (rans(:,3)-.5d0)*2.d0*(disc_thick*rans(:,1)) ! thickness is disk_thick*rad
-        rans(:,3) = (rans(:,3)-.5d0)*2.d0*disc_thick ! thickness is disk_thick
+        if ( rho_target<=0 .and. .not. constant_surf ) then
+            rans(:,3) = (rans(:,3)-.5d0)*2.d0*disc_thick ! thickness is disk_thick
+        endif
     
         p_data%r_p(1,:) = real(rans(:,1)*dsin(rans(:,2)))
         p_data%r_p(2,:) = real(rans(:,1)*dcos(rans(:,2)))
