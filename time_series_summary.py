942154e396e5231959ec2fc73112116d09e9c65d
David Williamson <d.j.williamson@soton.ac.uk>
2018-06-01 11:48:27 +0100

    allow switching off plots

--- time_series_summary.py
+++ time_series_summary.py
@@ -20,10 +20,10 @@ if __name__ == '__main__':
 #     run_ids     = ["1055","1055"]
 #     runs = ["q2_SN","q2_SN_slow"]
 #     names = ["ASS","AS"]
-    run_ids     = ["1058","1058"]
-    runs = ["restest0m02","restest0m04"]
-    names = ["0m02","0m04"]
-    outfile = "../figures/timeseries_summary_res.pdf"
+#     run_ids     = ["1058","1058"]
+#     runs = ["restest0m02","restest0m04"]
+#     names = ["0m02","0m04"]
+#     outfile = "../figures/timeseries_summary_res_fast.pdf"
 
 #     run_ids     = ["2014","2014","2015","2014","2015","2014","1055","1055","2018"]
 #     runs = ["q2redo","q2edd05redo","q2edd10_aniso1fixed","q2edd10redo","q2edd10_aniso3fixed","q2edd20redo","q2_SN_slow","q2_SN","treetest"]
@@ -31,6 +31,18 @@ if __name__ == '__main__':
 #     names = ["A","B","C1","C2","C3","D",r"A$_*$",r"A$_{**}$","X"]
 #     sfr_split = [["A","B","D","X"],[r"A$_*$",r"A$_{**}$"],["C1","C2","C3"]]
 #     outfile = "../figures/timeseries_summary_q2.pdf"
+
+    run_ids     = ["2014","2014","2015","2014","2015","2014","1055","1055"]
+    runs = ["q2redo","q2edd05redo","q2edd10_aniso1fixed","q2edd10redo","q2edd10_aniso3fixed","q2edd20redo","q2_SN_slow","q2_SN"]
+#     edd = [.01,.05,.1,.1,.1,.2] # incorrect anyway
+    names = ["A","B","C1","C2","C3","D",r"A$_*$",r"A$_{**}$"]
+    sfr_split = [["A","B","D","X"],[r"A$_*$",r"A$_{**}$"],["C1","C2","C3"]]
+#     sfr_split = None
+    outfile = "../figures/timeseries_summary_q2.pdf"
+    
+    plotDWind = False
+    plotSFR = True
+    plotAngle = False
 
 #     names = ["02","04","06","08","1"]
 #     run_ids = ["2019"]*len(names)
@@ -41,57 +53,80 @@ if __name__ == '__main__':
         nsfr_plots = len(sfr_split)
     else:
         nsfr_plots = 1
-    nsp = 2+nsfr_plots
+    nsp=0
+    if plotDWind: nsp+=1
+    if plotSFR:
+        sf_sp_offset = nsp
+        nsp+=nsfr_plots
+    if plotAngle:
+        angle_sp_offset = nsp
+        nsp+=1
+#     nsp = 2+nsfr_plots
     
 #     nsp = 5
 #     nsp = 3
 
-    fig,sp = P.subplots(nsp,1,figsize=(5,8.),sharex=True)
+    fig,sp = P.subplots(nsp,1,figsize=(5,2.*nsp),sharex=True)
+    if not isinstance(sp,np.ndarray):
+        sp = np.array([sp,None])
     
-    skiprate = 1
+    skiprate = 19
     
     linewidth = 1.5
 
     for irun,(run_id,output_dir) in enumerate(zip(run_ids,runs)):
         print("Plotting: ",output_dir)
         
-        sf_data = np.loadtxt("data/sf"+run_id+output_dir+".dat")
-        sf_sp = sp[1]
-        if sfr_split is not None:
-            for isplit,sfr_plotlist in enumerate(sfr_split):
-                if names[irun] in sfr_plotlist:
-                    sf_sp  = sp[isplit+1]
-        sf_sp.plot(sf_data[:,0][::skiprate],sf_data[:,2][::skiprate],'C'+str(irun),lw=linewidth)
+        if plotSFR:
+            sf_data = np.loadtxt("data/sf"+run_id+output_dir+".dat")
+            sf_sp = sp[sf_sp_offset]
+            if sfr_split is not None:
+                for isplit,sfr_plotlist in enumerate(sfr_split):
+                    if names[irun] in sfr_plotlist:
+                        sf_sp  = sp[isplit+sf_sp_offset]
+            sf_sp.plot(sf_data[:,0],sf_data[:,2],'C'+str(irun),lw=linewidth,label=names[irun])
         
         wind_data = np.loadtxt("data/windangle_evolution"+run_id+output_dir+".dat")
         good_slice = (wind_data[:,4]>0.)
-        sp[1+nsfr_plots].plot(wind_data[good_slice,1][::skiprate],wind_data[good_slice,2][::skiprate],lw=linewidth)
-        sp[0].plot(wind_data[good_slice,1][::skiprate],wind_data[good_slice,4][::skiprate],label=names[irun],lw=linewidth)
+        if plotAngle:
+            sp[angle_sp_offset].plot(wind_data[good_slice,1][::skiprate],wind_data[good_slice,2][::skiprate],lw=linewidth,label=names[irun])
+        if plotDWind:
+            sp[0].plot(wind_data[good_slice,1][::skiprate],wind_data[good_slice,4][::skiprate],label=names[irun],lw=linewidth)
 #         sp[3].plot(wind_data[:,1],wind_data[:,4]/edd[irun])
     
-    for isp in range(1,1+nsfr_plots):
+    for isp in range(sf_sp_offset,sf_sp_offset+nsfr_plots):
         sp[isp].set_ylabel(r"SFR (M$_\odot$/yr)")
         sp[isp].set_yscale('log')
-        sp[isp].set_ylim([1.e-5,.2])
+        sp[isp].set_ylim([1.e-5,1.])
+        sp[isp].set_yticks([1e-5,1e-4,1e-3,1e-2,1e-1])
 
-    sp[1+nsfr_plots].set_ylabel(r"Wind angle ($^\circ$)")
+    if plotAngle:
+        sp[angle_sp_offset].set_ylabel(r"Wind angle ($^\circ$)")
 
-    sp[0].set_ylabel(r"$\dotM_w$ (M$_\odot$/yr)")
-    sp[0].set_yscale('log')
+    if plotDWind:
+        sp[0].set_ylabel(r"$\dotM_w$ (M$_\odot$/yr)")
+        sp[0].set_yscale('log')
 
 #     sp[3].set_ylabel(r"$\dotM_w/f_\mathrm{edd}$ (M$_\odot$/yr)")
 #     sp[3].set_yscale('log')
     
-    for this_sp in sp:
-#         this_sp.set_xlim([0.,1.2])
-        this_sp.set_xlim([0.,0.2])
+    for this_sp in sp[0:nsp]:
+        this_sp.set_xlim([0.,1.2])
+#         this_sp.set_xlim([0.,0.2])
     
-    sp[0].set_ylim([1.e-5,.2])
+    if plotDWind:
+        sp[0].set_ylim([1.e-5,.2])
 #     sp[1+nsfr_plots].set_ylim([0.,14.])
-    sp[1+nsfr_plots].set_ylim([0.,None])
+    if plotAngle:
+        sp[angle_sp_offset].set_ylim([0.,16.])
 
     sp[nsp-1].set_xlabel("$t$ (Myr)")
-    sp[0].legend(loc='best',fontsize='xx-small')
+    if plotDWind:
+        sp[0].legend(loc='best',fontsize='xx-small')
+    else:
+        for isp in range(0,nsfr_plots):
+            sp[isp].legend(loc='best',fontsize='xx-small')
+
 #     sp[0].legend(loc='best')
     
     P.tight_layout()
